7aa05ad60ee1fbdf2ff483003c7eb45d36cbaecf
NunezAN <alex101nunez@gmail.com>
2023-02-18 22:07:35 -0600

    setup inital data in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="text-red-300">Test</div>;
+  const [items, setItems] = useState(["A", "B", "C"]);
+  return (
+    <div className="bg-black h-screen w-full text-white pt-8">
+      <div className="flex flex-row gap-4 max-w-3xl mx-auto items-center justify-center">
+        {items.map((item) => (
+          <div className="border-2 p-4 rounded-xl text-2xl">{item}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
